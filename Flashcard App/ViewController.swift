7d25a44922217e49b27754ca39c7419960b12e46
iD Student <idstudent@iDs-iMac.local>
2017-08-02 15:14:02 -0700

    I created variables for questions and answers and connected them to the main storyboard.

--- Flashcard App/ViewController.swift
+++ Flashcard App/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerPickerView: UIPickerView!
+    @IBOutlet weak var questionTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        answerPickerView.dataSource = self
+        answerPickerView.delegate = self
+        
+        setupCardUI()
+        
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setupCardUI() {
+        questionTextView.text = CardCollection.instance.currentCard.question
+        questionLabel.text = "Question \(CardCollection.instance.currentIndex + 1)/\(CardCollection.instance.cards.count)"
+    }
+    
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return CardCollection.instance.currentCard.options.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return CardCollection.instance.currentCard.options[row];
+    }
+    
 }
-
